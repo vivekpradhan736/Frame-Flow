c40d5fc299ee053c55d316973d344b68ae7e4f7d
Vivek <vivekpradhan736@gmail.com>
2025-02-13 14:16:09 +0530

    new commit

--- app/(routes)/designs/page.tsx
+++ app/(routes)/designs/page.tsx
@@ -4,21 +4,23 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import DesignCard from './_components/DesignCard';
 import { RECORD } from '@/app/view-code/[uid]/page';
+import { Skeleton } from "@/components/ui/skeleton"
 
 function Designs() {
 
     const { user } = useAuthContext();
+    const [loading, setLoading] = useState(true);
     const [wireframeList, setWireframeList] = useState([]);
     useEffect(() => {
         user && GetAllUserWireframe();
     }, [user])
 
     const GetAllUserWireframe = async () => {
-
+        // setLoading(true)
         const result = await axios.get('/api/wireframe-to-code?email='
             + user?.email);
-        console.log(result.data);
-        setWireframeList(result.data);
+            setWireframeList(result.data);
+            setLoading(false)
     }
 
     return (
@@ -26,8 +28,18 @@ function Designs() {
             <h2 className='font-bold text-2xl'>Wireframe & Codes</h2>
 
             <div className='grid grid-cols-2 lg:grid-cols-3 gap-7 mt-10'>
-                {wireframeList?.map((item: RECORD, index) => (
-                    <DesignCard key={index} item={item} />
+                {loading === true && (
+                    <>
+                      <Skeleton className="h-[225px] w-full rounded-lg" />
+                      <Skeleton className="h-[225px] w-full rounded-lg" />
+                      <Skeleton className="h-[225px] w-full rounded-lg" />
+                      <Skeleton className="h-[225px] w-full rounded-lg" />
+                      <Skeleton className="h-[225px] w-full rounded-lg" />
+                      <Skeleton className="h-[225px] w-full rounded-lg" />
+                    </>
+                )}
+                {loading === false && wireframeList?.map((item: RECORD, index) => (
+                        <DesignCard key={index} item={item} />
                 ))}
             </div>
         </div>
